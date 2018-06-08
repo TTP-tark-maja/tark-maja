50727cc14f923fe43a34d30ee0973122a6480728
Simone Niinemägi <31885412+niinsimo@users.noreply.github.com>
2018-06-08 11:23:24 +0300

    Convert timestamp to time

--- elering-latest-price-to-file.py
+++ elering-latest-price-to-file.py
@@ -1,8 +1,17 @@
+import time
 import urllib.request, json 
 #Päringu saime https://dashboard.elering.ee/documentation.html lehelt.
 with urllib.request.urlopen("https://dashboard.elering.ee/api/nps/price/ee/latest") as url:
-    data = json.loads(url.read().decode())
-    print(data)
-    
+    #võtab eleringist stringina viimase hinna ja kuupäeva
+    jsonstring = json.loads(url.read().decode())
+    data =(jsonstring['data'])
+    string = data[0]
+    time = time.ctime(string['timestamp'])
+    price = string['price']
+    print(time)
+    print(price)
+
+#kirjutab andmed faili    
 with open('data.txt', 'w') as outfile:  
-    json.dump(data, outfile)
+    json.dump(time, outfile)
+    json.dump(', '+price, outfile)
