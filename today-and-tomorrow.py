6dd7474e6c2cfc5a44ccdc36448a896464d187cb
Robert Roos <31948767+roosen7@users.noreply.github.com>
2018-06-12 11:53:52 +0300

    takes todays 24h prices from Elering

--- today-and-tomorrow.py
+++ today-and-tomorrow.py
@@ -1,20 +1,23 @@
-from datetime import date
-today = str(date.today())
-print(today)
 import time
 import datetime
 import urllib.request, json
 import os
-import pytz
-
+from datetime import date, timedelta
+today = str(date.today())
+yesterday = str(date.today() - timedelta(1))
+print(yesterday)
+print(today)
 
 today = str(date.today())
 tomorrow = datetime.date.today() + datetime.timedelta(days=1)
 tomorrow1 = tomorrow.strftime('%Y-%m-%d')
+yesterday = date.today() - timedelta(1)
+yesterday1 = yesterday.strftime('%Y-%m-%d')
 text = 'https://dashboard.elering.ee/api/nps/price?start='
 text2 = '&end='
 #Päringu saime https://dashboard.elering.ee/documentation.html lehelt.
-with urllib.request.urlopen(text+today+text2+tomorrow1) as url:
+with urllib.request.urlopen(text+yesterday1+text2+tomorrow1) as url:
+    #print(text+yesterday1+text2+tomorrow1)
     #võtab eleringist stringina tänased hinnad
     jsonstring = json.loads(url.read().decode())
     data =(jsonstring['data'])
@@ -24,7 +27,7 @@ with urllib.request.urlopen(text+today+text2+tomorrow1) as url:
 
         now = datetime.datetime.now()
 
-        for i in range (0, 22) :
+        for i in range (21, 46) :
             eestiajad =(eesti[i])
             kellaaeg = (eestiajad['timestamp'])
             price = (eestiajad['price'])
