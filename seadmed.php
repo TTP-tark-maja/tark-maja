c5ef8dfc0a5b5a6d1044b41a9c512a17dcb7d0d9
Robert Roos <31948767+roosen7@users.noreply.github.com>
2018-06-15 13:07:36 +0300

    php added

--- seadmed.php
+++ seadmed.php
@@ -14,25 +14,76 @@
 			<li><a href="seadmed.php">Seadmed</a></li>
         </ul>
     </nav>
-        <h1>Seadmed</h1>
+    
+    <h1>seadmed</h1>
+        <br>  
+    <div class="main-grid">
+        <div class="header-uks">
+          <h2>Seadmed</h2>
+		  <?php
+		if(isset($_POST['seadme_nimetus']) && isset($_POST['seadme_voimsus'])) {
+			$data = $_POST['seadme_nimetus'] . ' - ' . $_POST['seadme_voimsus'] ."W"."\n";
+			$ret = file_put_contents('seadmed.txt', $data, FILE_APPEND | LOCK_EX);
+		}
+
+		$myfile = fopen("seadmed.txt", "r") or die("ei saa avada faili!");
+				while(!feof($myfile)) {
+				echo fgets($myfile) . "<br>";
+				}
+				fclose($myfile);
+				
+		// change the name below for the folder you want
+		$dir = "new_folder_name";
+
+		$file_to_write = 'test.txt';
+		$content_to_write = "The content";
+
+		if( is_dir($dir) === false )
+		{
+			mkdir($dir);
+		}
+
+		$file = fopen($dir . '/' . $file_to_write,"w");
+
+		// a different way to write content into
+		// fwrite($file,"Hello World.");
+
+		fwrite($file, $content_to_write);
+
+		// closes the file
+		fclose($file);
+
+		// this will show the created file from the created folder on screen
+		include $dir . '/' . $file_to_write;
+		?>
+        </div>
+
+        <div class="loetelu">
+
+        <br>
+            
+        </div>
+	<form method="POST">
+        <div class="header-kaks">
+            <h2>Lisa seade</h2>
+        </div>
+
+        <div class="lisamine">
 		
-    <div class="container">     
-        <form>
-			<ol id="dynamic-list"></ol><br>
-			<input type="text" id="candidate"/>
-				<div class="button">
-				<script src="pakett.js"></script><br>
-					<input onclick="addItem();" type="button" value="LISA" name="answer" id="answer">
-					<input onclick="removeItem()" type="button" value="EEMALDA">
-					<?php
-					$fname = "answer.txt";
-					$result = isset($_GET['answer']);
-					$prev = file_get_contents($fname);
-					$result = $result.",".$prev;
-					file_put_contents($fname,$result);
-					?>
-				</div> 
-        </form>
-    </div>        
+            <legend>Nimetus</legend>
+                <input type="text" name="seadme_nimetus"><br>
+            <br>
+            <legend>Võimsus (W)</legend>
+                <input type="text" name="seadme_voimsus"><br>
+			<br>
+            <div class="submit">
+                <input type="submit" value="SALVESTA">
+            </div>
+        </div>
+    </div>
+    </form>
 </body>
 </html>
+
+
+
