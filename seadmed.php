506e50e6d50316de45f3238e5e59d05c0719e6f2
Simone Niinemägi <31885412+niinsimo@users.noreply.github.com>
2018-06-18 15:55:12 +0300

    Premission to delete files

--- seadmed.php
+++ seadmed.php
@@ -3,53 +3,108 @@
 <head>
     <meta charset="utf-8">
     <title>Tark Maja</title>
-    <link rel="icon" type="image/png" href="M.png">
     <link rel="stylesheet" type="text/css" href="design/style.css">
 </head>
-<body class="page_bg">
+<body>
     <nav>
         <ul>
-            <li><a href="koduleht.php">Avaleht</a></li>
+            <li><a href="koduleht.php">Home</a></li>
             <li><a href="paketiinfo.html">Paketi informatsioon</a></li>
+            <li><a href="tingimused.html">Seadme tingimused</a></li>
+			<li><a href="seadmed.php">Seadmed</a></li>
         </ul>
     </nav>
+    
+    <h1>seadmed</h1>
+        <br>  
+    <div class="main-grid">
+        <div class="header-uks">
+          <h2>Seadmed</h2>
+		  <?php
+		 foreach (scandir('users/') as $dir){
+			echo "<a href=users/$dir>$dir</a>" . "<br>";
+		}
+		echo "<a href='./' style='display:none;' >.</a>";
+		if($_SERVER["REQUEST_METHOD"] == "POST"){
+			if(isset($_POST['seadme_nimetus']) && isset($_POST['seadme_voimsus'])) {
+				$dir = $_POST['seadme_nimetus'];
+				$power = $_POST['seadme_voimsus'];
+				$file_to_write1 = "logfile.txt";
+				$file_to_write2 = "tingimused.txt";
+				$file_to_write3 = "WorkingTimes.txt";
+				$file_to_write4 = "TurnOffTimes.txt";
+				$file_to_write5 = "todaydata.txt";
+				$file_to_write6 = "seadme_info.txt";
+				
+				if( is_dir($dir) === false ) {
+					mkdir('users/'. $dir);
+					mkdir('users/'. $dir . '/design');
+					$file1 = fopen('users/'.$dir. '/' .$file_to_write1, "w");
+					$file2 = fopen('users/'.$dir. '/' .$file_to_write2, "w");
+					$file3 = fopen('users/'.$dir. '/' .$file_to_write3, "w");
+					$file4 = fopen('users/'.$dir. '/' .$file_to_write4, "w");
+					$file5 = fopen('users/'.$dir. '/' .$file_to_write5, "w");
+					$file6 = fopen('users/'.$dir. '/' .$file_to_write6, "w");
+					copy("Arvutaja.py", "users/$dir/Arvutaja.py");
+					copy("tingimused_html.php", "users/$dir/index.html");
+					copy("design/style.css", "users/$dir/design/style.css");
+					copy("4.png", "users/$dir/4.png");
+					fwrite($file1, "");
+					fwrite($file2, "");
+					fwrite($file3, "");
+					fwrite($file4, "");
+					fwrite($file5, "");
+					fwrite($file6, "Seadme nimetus:". $dir ."\n Seadme võimsus:". $power);
+					chmod('users/' . $dir. '/' .$file_to_write1, 0777);
+					chmod('users/' . $dir . '/' . $file_to_write2, 0777);
+					chmod('users/' . $dir, 0777);
+					chmod('users/' . $dir. '/' .$file_to_write3, 0777);
+					chmod('users/' . $dir. '/' .$file_to_write4, 0777);
+					chmod('users/' . $dir. '/' .$file_to_write5, 0777);
+					chmod('users/' . $dir. '/' .$file_to_write6, 0777);
+					
+					echo "<a href='users/$dir'>$dir</a>";
+				}
+				
+			}
+			else {
+				echo "viga";
+			}
+		}
+		
+				
+		?>
+        </div>
 
-    <div class="banner">
-        <img class="banner-image" src="4.JPG">
-     </div><hr>
+        <div class="loetelu">
 
-     <h1>Tarbija</h1>
+        <br>
+            
+        </div>
+	<form method="POST">
+        <div class="header-kaks">
+            <h2>Lisa seade</h2>
+        </div>
 
-
-    <div class="container">
-            <div class="button">
-                <input type="button" value="Seadme tingimused">
+        <div class="lisamine">
+		
+            <legend>Nimetus</legend>
+                <input type="text" name="seadme_nimetus"><br>
+            <br>
+            <legend>Võimsus (W)</legend>
+                <input type="text" name="seadme_voimsus"><br>
+			<br>
+            <div class="submit">
+                <input type="submit" value="SALVESTA">
             </div>
-        <h2>seadme olek</h2>
-            <label class="switch">
-                <input type="checkbox">
-                <span class="slider"></span>
-            </label><br><br>
-            <h2>seadme logi</h2>
-                    <?php
-                    $lines = file('logfile.txt');
-                    $first5 = array_slice($lines, 0, 6);
-                    echo implode("<p></p>", $first5);
-                    
-                    if(isset($_POST['kogulogi']))
-                    {
-                            $content = file('logfile.txt');
-                            foreach ($content as $line){ 
-                                echo ('<li>' . $line . '</li>');
-                            }
-                    }
-                ?> 
-        <form method="POST">
-		   <div class="submit">
-		   <br>
-                <input type="submit" value="KUVA KOGU LOGI" name="kogulogi" id="1">
-           </div> 
-        </form>
-    </div> 
+        </div>
+    </div>
+    </form>
 </body>
 </html>
+
+
+
+
+
+
